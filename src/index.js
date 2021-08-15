6c4a1bc31bd7dda2f96dc0176878d688baa80f6e
ManzDev <JoseRomanHernandez@gmail.com>
2021-08-15 13:20:28 +0100

    Add timers JS

--- src/index.js
+++ src/index.js
@@ -3,22 +3,22 @@ const powerButton = document.querySelector(".power-switch .button");
 const eyes = document.querySelector(".eyes");
 const screen = document.querySelector(".screen");
 
+const timers = [];
+
 powerButton.addEventListener("click", () => {
   powerLed.classList.toggle("on");
 
   if (powerLed.classList.contains("on")) {
+    timers.splice(0);
+    screen.classList.remove("off");
     startTyping(["cd manz.dev", "npm install", "npm run dev"]);
   } else {
     eyes.classList.add("off");
     screen.innerHTML = "";
+    timers.forEach(timer => clearTimeout(timer));
+    timers.splice(0);
   }
 });
-
-// document.addEventListener("keydown", (ev) => {
-//   if (ev.key.toLowerCase() === "e") {
-//     eyes.classList.toggle("off");
-//   }
-// });
 
 const startTyping = (texts) => {
   let wait = 750;
@@ -27,21 +27,25 @@ const startTyping = (texts) => {
   texts.forEach(text => {
     wait += 750;
     for (let i = 0; i < text.length; i++) {
-      setTimeout(() => {
+      const timer = setTimeout(() => {
         screen.innerHTML += text[i];
       }, wait);
+      timers.push(timer);
       wait += 50 + ~~(Math.random() * 50);
     }
 
     wait += 750;
 
-    setTimeout(() => (screen.innerHTML += "<br>$ "), wait);
+    const timer = setTimeout(() => (screen.innerHTML += "<br>$ "), wait);
+    timers.push(timer);
   });
 
   wait += 500;
 
-  setTimeout(() => {
-    setTimeout(() => eyes.classList.remove("off"), 500);
+  const timer = setTimeout(() => {
+    const timer = setTimeout(() => eyes.classList.remove("off"), 500);
+    timers.push(timer);
     screen.classList.add("off");
   }, wait);
+  timers.push(timer);
 };
